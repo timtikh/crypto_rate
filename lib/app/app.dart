ed2452d5680aaf98f41545c749f8966deb20e591
timtikh <alfertikhonov@yandex.ru>
2025-08-18 20:31:21 +0300

    dio added api_keys added and hided debug banner hided

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -14,6 +14,7 @@ class MyApp extends ConsumerWidget {
     final authState = ref.watch(authStateChangesProvider);
 
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
       ),
