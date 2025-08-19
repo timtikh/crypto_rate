414c7e19d64cc8c7c228985bfdbdb6b88c60352c
timtikh <alfertikhonov@yandex.ru>
2025-08-19 14:45:35 +0300

    double login bug corrected so it is protected

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -4,26 +4,40 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../pages/home_page.dart';
 import '../pages/login/presentation/login_page.dart';
 import '../pages/login/providers/auth_providers.dart';
-import '../pages/rates/rates_page.dart';
 
 class MyApp extends ConsumerWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final authState = ref.watch(authStateChangesProvider);
+    final navigatorKey = GlobalKey<NavigatorState>();
+
+    ref.listen(authStateChangesProvider, (previous, next) {
+      next.whenData((user) {
+        WidgetsBinding.instance.addPostFrameCallback((_) {
+          if (user == null) {
+            navigatorKey.currentState?.pushAndRemoveUntil(
+              MaterialPageRoute(builder: (_) => const LoginPage()),
+                  (route) => false,
+            );
+          } else {
+            navigatorKey.currentState?.pushAndRemoveUntil(
+              MaterialPageRoute(builder: (_) => const HomePage()),
+                  (route) => false,
+            );
+          }
+        });
+      });
+    });
 
     return MaterialApp(
+      navigatorKey: navigatorKey,
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
       ),
-      home: authState.when(
-        data: (user) => user == null ? const LoginPage() : const HomePage(),
-        loading: () => const Scaffold(
-          body: Center(child: CircularProgressIndicator()),
-        ),
-        error: (e, _) => Scaffold(body: Center(child: Text("Error: $e"))),
+      home: const Scaffold(
+        body: Center(child: CircularProgressIndicator()),
       ),
     );
   }
